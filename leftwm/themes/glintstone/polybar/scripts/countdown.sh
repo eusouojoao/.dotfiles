ca41ca1fbff767ccd65f458776505b44cb03731d
eusouojoao <81173835+eusouojoao@users.noreply.github.com>
2022-03-13 18:08:22 +0000

    *

--- leftwm/themes/glintstone/polybar/scripts/countdown.sh
+++
@@ -1,3 +0,0 @@
-#bin/bash
-# Days left until Elden Ring releases :)
-echo "Elden Ring -- countdown: $((($(date +%s --date "2022-2-25")-$(date +%s))/(3600*24*7))) weeks and $((($(date +%s --date "2022-2-25")-$(date +%s))/(3600*24) - 7*(($(date +%s --date "2022-2-25")-$(date +%s))/(3600*24*7)))) days"
